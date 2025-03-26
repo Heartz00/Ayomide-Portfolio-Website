c2efa9f3b96077e5a7517e05b7e0fd54f6d1476a
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-03-26 18:01:15 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/AyomideProfile.jpg";
+import profilePic from "../assets/AyomideProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
