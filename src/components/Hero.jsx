620ef61962e3a525eac93cd421d5f8a8f3546850
Ayomide Benjamen Oladele <oladeleayomide831@gmail.com>
2026-09-19 14:43:06 -0500

    updated profile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
               whileTap={{ scale: 0.95 }}
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Software Engineer - Backend Focus
+              Software  and Data Engineer
             </motion.span>
 
             <motion.p
